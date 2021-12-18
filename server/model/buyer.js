41098596e3252b3fd88436d835ca214446480301
vidhitapai <vidhitapai@gmail.com>
2021-12-18 19:04:49 +0530

    Added phoneOTP field

--- server/model/buyer.js
+++ server/model/buyer.js
@@ -3,33 +3,39 @@ const mongoose = require("mongoose");
 const validate = require("validator");
 
 // Creating the schema
-const buyerSchema =  new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validate.isEmail(value)) {
-                throw new Error('Invalid email');
+const buyerSchema =  new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if(!validate.isEmail(value)) {
+                    throw new Error('Invalid email');
+                }
             }
-        }
+        },
+        phone: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            maxlength: [10, "Invalid phone number"],
+            minlength: [10, "Invalid phone number"]
+        },
+        phoneOTP: {
+            type: String
+        },
     },
-    phone: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        maxlength: [10, "Invalid phone number"],
-        minlength: [10, "Invalid phone number"]
-    }
-});
+    { timestamps: true }
+);
 
 const Buyer = mongoose.model('Buyer',buyerSchema);
 
