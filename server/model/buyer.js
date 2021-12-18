0412b6e17e72959f69cf2da1bf24418ef2c5e8cf
Rosita <esorthe7@gmail.com>
2021-12-19 01:36:07 +0530

    Add EmailOtp to Schema

--- server/model/buyer.js
+++ server/model/buyer.js
@@ -3,42 +3,43 @@ const mongoose = require("mongoose");
 const validate = require("validator");
 
 // Creating the schema
-const buyerSchema =  new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if(!validate.isEmail(value)) {
-                    throw new Error('Invalid email');
-                }
-            }
-        },
-
-        phone: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-            maxlength: [10, "Invalid phone number"],
-            minlength: [10, "Invalid phone number"]
-        },
-
-        phoneOTP: {
-            type: String
-        }
+const buyerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
 
-    { timestamps: true }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validate.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      maxlength: [10, "Invalid phone number"],
+      minlength: [10, "Invalid phone number"],
+    },
+
+    phoneOTP: {
+      type: Number,
+    },
+    emailOTP: {
+      type: Number,
+    }
+  },
+  { timestamps: true }
 );
 
 const Buyer = mongoose.model('Buyer',buyerSchema);
