d9e819507857bc1610762bc835c20f6ffd6c0c5b
kc <kc.dark@protonmail.com>
2021-12-18 17:25:50 +0530

    created a user schema

---
+++ server/model/buyer.js
@@ -0,0 +1,37 @@
+// Importing required modules
+const mongoose = require("mongoose");
+const validate = require("validator");
+
+// Creating the schema
+const buyerSchema =  new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validate.isEmail(value)) {
+                throw new Error('Invalid email');
+            }
+        }
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        maxlength: [10, "Invalid phone number"],
+        minlength: [10, "Invalid phone number"]
+    }
+});
+
+const Buyer = mongoose.model('Buyer',buyerSchema);
+
+// Exporting the module
+module.exports = Buyer;
