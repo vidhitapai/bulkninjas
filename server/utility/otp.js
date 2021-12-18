98f5b2f5e08f20906feb26924e7ae6c6fc427123
Rosita <esorthe7@gmail.com>
2021-12-19 01:35:14 +0530

    Add Email verification through OTP

--- server/utility/otp.js
+++ server/utility/otp.js
@@ -2,6 +2,7 @@
 const res = require("express/lib/response");
 const fast2sms = require("fast-two-sms");
 const dotenv = require('dotenv').config();
+const nodemailer = require("nodemailer");
 
 // Generating OTP
 const generateOTP = (otpLength) => {
@@ -14,7 +15,7 @@ const generateOTP = (otpLength) => {
 };
 
 // Sending OTP via SMS
-const sendSMS = async ({ message, contactNumber }) => {
+const sendSMS = async ({ message, contactNumber}) => {
     try {
         const res = await fast2sms.sendMessage({
             authorization: process.env.API_KEY,
@@ -30,8 +31,45 @@ const sendSMS = async ({ message, contactNumber }) => {
     }
 };
 
+const sendEmail = async ({message, emailId}) => {
+    try {
+       let transporter = nodemailer.createTransport({
+         service: "gmail",
+         auth: {
+           type: "OAuth2",
+           user: process.env.MAIL_USERNAME,
+           pass: process.env.MAIL_PASSWORD,
+           clientId: process.env.OAUTH_CLIENT_ID,
+           clientSecret: process.env.OAUTH_CLIENT_SECRET,
+           refreshToken: process.env.OAUTH_REFRESH_TOKEN
+         },
+       });
+       let mailOptions = {
+         from: "socialmigraine@gmail.com",
+         to: emailId,
+         subject: "OTP for Registration",
+         text: message
+       };
+       transporter.sendMail(mailOptions, function (err, data) {
+         if (err) {
+           console.log("Error " + err);
+         } else {
+           console.log("Email sent successfully");
+         }
+       });
+    } catch (error) {
+      res.status(400).json({
+        message: error.message,
+      });
+      console.log(error);
+    }
+}
+
+
+
 // Exporting modules
 module.exports = {
     generateOTP,
-    sendSMS
+    sendSMS,
+    sendEmail
 }
