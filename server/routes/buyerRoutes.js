fa99f8035e9ef441a3d65ada185c679c126e5e09
vidhitapai <vidhitapai@gmail.com>
2021-12-19 12:00:40 +0530

    Added otp verification routes

--- server/routes/buyerRoutes.js
+++ server/routes/buyerRoutes.js
@@ -1,10 +1,20 @@
 // Importing modules
 const express = require("express");
 const router = express.Router();
-const { createNewBuyer } = require("../controller/authController.js");
+const { 
+    createNewUser,
+    verifyPhoneOTP,
+    verifyEmailOTP
+} = require("../controller/authController.js");
 
-// Route to register a new buyer
-router.post("/register", createNewBuyer);
+// Route to register a new user
+router.post("/register", createNewUser);
+
+// Route to authenticate phone OTP
+router.post("/authenticate/phoneotp", verifyPhoneOTP);
+
+// Route to authenticate email OTP
+router.post("/authenticate/emailotp", verifyEmailOTP);
 
 // Exporting modules
 module.exports = router;
