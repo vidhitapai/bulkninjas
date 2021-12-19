5cbc2257b50d31eef7026716b75192440281a01f
kc <kc.dark@protonmail.com>
2021-12-19 10:59:46 +0530

    Added JWT generation & verification

---
+++ server/utility/token.js
@@ -0,0 +1,27 @@
+// Import modules
+const jwt = require("jsonwebtoken");
+const dot = require("dotenv").config();
+
+// Generate JWT Token 
+const generateJWT = (payload) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "12h"});
+    return token;
+};
+
+// Verify JWT Token
+const verifyJWT = (token) => {
+    try {
+        const { buyerID } = jwt.verify(token, process.env.JWT_SECRET);
+        return buyerID;
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+// Export modules
+module.exports = {
+    generateJWT,
+    verifyJWT
+}
