37105706004335af4d1d21f9477deb99f823f6c1
Kushmaniar <kushmaniar@gmail.com>
2021-12-20 03:41:31 +0530

    navbar 1

---
+++ client/src/components/landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Box} from '@mui/material';
+function landing() {
+
+    return(
+        <Box className='row'>
+        <Box className='landing-column'>
+
+        </Box>
+        <Box className='landing-column'>
+           
+        </Box>
+        </Box>
+    );
+
+}
