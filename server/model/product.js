f5e76701fedcc05fd787f300fd910626f823ed25
vidhitapai <vidhitapai@gmail.com>
2021-12-19 21:04:13 +0530

    Minor change

--- server/model/product.js
+++ server/model/product.js
@@ -20,8 +20,7 @@ const productSchema = new mongoose.Schema(
         },
 
         picture: {
-            type: Buffer,
-            required: true
+            type: Buffer
         },
 
         totalQuantity: {
