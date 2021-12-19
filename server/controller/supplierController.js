5063b950c2915f7621c0c407c52c92f0f1b442fe
kc <kc.dark@protonmail.com>
2021-12-19 21:22:33 +0530

    Added product picture upload controller

--- server/controller/supplierController.js
+++ server/controller/supplierController.js
@@ -1,8 +1,11 @@
 // Importing modules
-const product = require("../model/product.js");
+const multer = require('multer');
+const sharp = require('sharp');
+const Product = require("../model/product.js");
 
-const uploadProduct = async (res, req) => {
-    const newProduct = req.body;
+// <-------------------- Create New Product Listing -------------------->
+const createNewProductListing = async (res, req) => {
+    const newProduct = new Product(req.body);
     try {
         await newProduct.save();
 
@@ -16,3 +19,43 @@ const uploadProduct = async (res, req) => {
         });
     }
 };
+
+
+//
+const upload =  multer({
+    limits: {
+      fileSize: 1500000
+    },
+    fileFilter(req, file, callback) {
+        if(!file.originalname.match(/\.jpg|jpeg|png/)) {
+            return callback(new Error('Please upload the correct file format!'));
+        }
+        callback(undefined, true);
+    }
+});
+
+
+// <-------------------- Uploading Product Picture -------------------->
+const uploadProductPicture = async (req, res) => {
+    try {
+        const product = await Product.findById(req._id);
+        const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+        product.picture = buffer;
+        await product.save();
+        res.status(201).json({
+            message: "Picture Successfully Uploaded"
+        });
+    }
+    catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+// Exporting modules
+module.exports = {
+    createNewProductListing,
+    upload,
+    uploadProductPicture
+}
