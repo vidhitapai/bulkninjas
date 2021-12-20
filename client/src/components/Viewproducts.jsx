23eba15e6e738eac5cfec08855af81cc4ac2986a
Kushmaniar <kushmaniar@gmail.com>
2021-12-21 04:47:33 +0530

    added new components and styling

--- client/src/components/Viewproducts.jsx
+++ client/src/components/Viewproducts.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
-import {Card, Button, Row, Col, Container} from 'react-bootstrap'
-function Viewproducts(props){
+import Productcard from './Productcard'
+function Viewproducts() {
+    
+    const array = [
+        {
+            Title: "Pen",
+            Price: "Rs. 10",
+            Quantity: "1000",
+            New_Price: "Rs. 8"
+        },
+        {
+            Title: "Bag",
+            Price: "Rs. 500",
+            Quantity: "100",
+            New_Price: "Rs. 375"
+        },
+        {
+            Title: "Car",
+            Price: "Rs. 10,00,000",
+            Quantity: "20",
+            New_Price: "Rs. 9,20,000"
+        },
+        {
+            Title: "Fridge",
+            Price: "Rs. 69,000",
+            Quantity: "100",
+            New_Price: "Rs. 42,000"
+        }
+    ];
+    
     return (
-        <>
-           <Card style={{ width: '35rem',
-                          backgroundColor: 'white',
-                          border:'1px solid black',
-                          padding: '15px',
-                          borderRadius: '10px',
-                          margin: '5rem ',
-                          marginTop: '10rem',
-                          justifyContent: 'center',
-                          alignItems: 'center'
-            }}>
-  <Card.Img variant="top" className="cardimg" src="https://images.unsplash.com/photo-1617503752587-97d2103a96ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=786&q=80" />
-  <Card.Body>
-    <Card.Title> Product Name</Card.Title>
-    <Card.Text>
-  <Container fluid>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    </Row>
-    <Row>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-        </>
-    )
+        <div>
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+        </div>
+    );
 }
 
 export default Viewproducts
